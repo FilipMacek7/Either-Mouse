b16ac702a37fd69d5c0c59f0ff10ad2f62e64d51
maceklu161 <maceklu161@DST20703>
2019-04-17 12:27:28 +0200

    init

---
+++ EitherMouse/EitherMouse/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EitherMouse
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [DllImport("User32.dll")]
+        static extern bool SystemParametersInfo(
+        uint uiAction,
+        uint uiParam,
+        uint pvParam,
+        uint fWinIni);
+
+        uint initialSpeed = SPI_SETMOUSESPEED;
+        public const uint SPI_SETMOUSESPEED = 0x0071;
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            SystemParametersInfo(SPI_SETMOUSESPEED, 0, initialSpeed, 0);
+        }
+    }
+}
---
+++ EitherMouse/EitherMouse/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EitherMouse
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const uint SPI_SETMOUSESPEED = 0x0071;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mousespeedtext.Text = "Mouse speed: " + SPI_SETMOUSESPEED;
+            speedslider.Value = SPI_SETMOUSESPEED;
+        }
+
+
+        [DllImport("User32.dll")]
+        static extern bool SystemParametersInfo(
+            uint uiAction,
+            uint uiParam,
+            uint pvParam,
+            uint fWinIni);
+
+
+        private void SpeedValue_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mousespeedtext.Text = "Mouse speed: " + speedslider.Value;
+            SystemParametersInfo(SPI_SETMOUSESPEED, 0, (uint)speedslider.Value, 0);
+        }
+        private void SpeedValue_Completed(object sender, DragCompletedEventArgs e)
+        {
+            SystemParametersInfo(SPI_SETMOUSESPEED, 0, (uint)speedslider.Value, 0);
+        }
+
+    }
+}
